50daa142278efc8949646d4f11f67f0a4daeb6e4
Santiago Cordova <A01029211@tec.mx>
2025-08-21 11:19:26 -0600

    Places Terminado

---
+++ SantiagoCordova_Suift/PlacesNuevo/PlacesNuevo/PlacesNuevoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesNuevoApp.swift
+//  PlacesNuevo
+//
+//  Created by Alumno on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PlacesNuevoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
